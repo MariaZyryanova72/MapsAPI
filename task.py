a44be7bb9d6964d6db42a09985217cc518310724
Zyryanova Maria <mariazyranova72@gmail.com>
2020-04-28 20:44:31 +0500

    Добавлен переключатель слоёв карты (схема/спутник/гибрид), при изменении которого меняется вид карты.

--- task.py
+++ task.py
@@ -5,19 +5,40 @@ from PyQt5 import uic
 import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QButtonGroup
 
 
 class MapApi(QWidget):
     def __init__(self):
         super().__init__()
+        self.map_file = "map.png"
         uic.loadUi('untitled.ui', self)
         self.initUI()
+
 
     def initUI(self):
         self.setWindowTitle('Отображение карты')
         self.pull_button.clicked.connect(self.getImage)
         self.label_error.setStyleSheet('color: red')
+        self.radioButton_map.setChecked(True)
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.radioButton_map)
+        self.button_group.addButton(self.radioButton_sat)
+        self.button_group.addButton(self.radioButton_satskl)
+
+        self.button_group.buttonClicked.connect(self._on_radio_button_clicked)
+
+    def _on_radio_button_clicked(self, button):
+        if button.text() == "Схема":
+            self.type_map = "map"
+            self.map_file = "map.png"
+        elif button.text() == "Спутник":
+            self.type_map = "sat"
+            self.map_file = "map.jpg"
+        elif button.text() == "Гибрид":
+            self.type_map = "sat,skl"
+            self.map_file = "map.jpg"
+        self.getImage()
 
     def getImage(self):
         if self.error():
@@ -40,7 +61,7 @@ class MapApi(QWidget):
 
             map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.lineEdit_x.text()}," \
                           f"{self.lineEdit_y.text()}&spn={self.lineEdit_spn.text()}," \
-                          f"{self.lineEdit_spn.text()}&l=map"
+                          f"{self.lineEdit_spn.text()}&l={self.type_map}"
             response = requests.get(map_request)
 
             if not response:
@@ -50,9 +71,10 @@ class MapApi(QWidget):
                 sys.exit(1)
 
             # Запишем полученное изображение в файл.
-            self.map_file = "map.png"
+
             with open(self.map_file, "wb") as file:
                 file.write(response.content)
+            print(self.map_file)
             self.pixmap = QPixmap(self.map_file)
             self.image.setPixmap(self.pixmap)
 
